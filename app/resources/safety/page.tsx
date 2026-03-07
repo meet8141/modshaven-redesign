4e1ab91eabcee9ed4bf9cf0be1134422712d57f0
meet8141 <meetpatel814116@gmail.com>
2026-03-07 18:14:12 +0530

    fix: remove unused Grainient import from multiple pages

--- app/resources/safety/page.tsx
+++ app/resources/safety/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import SplitText from "@/app/components/SplitText";
 import DecryptedText from '@/app/components/DecryptedText';
-import Grainient from '@/app/components/Grainient';
+
 
 
 
